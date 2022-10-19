e09a121d2378687c2625d210db618aeca38299ac
Bulat Khakimov <khakimovbm@gmail.com>
2022-10-19 09:54:13 +0300

    exe_ch_5

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -18,5 +18,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+
+    get signup_path
+    assert_template 'users/new'
+    assert_select "h1", text: "Sign up"
+
+    get signup_path
+    assert_select "title", full_title("Sign up")
+
   end
 end
